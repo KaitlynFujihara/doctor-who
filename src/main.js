e208c368f40bf7d5cdf9fd0d8753313cdfd254c0
Guest User <Guest@Epicodus-6D.local>
2018-04-02 09:44:52 -0700

    fix last bug about error message

--- src/main.js
+++ src/main.js
@@ -7,34 +7,28 @@ $(document).ready(function() {
   function searchDoctors(searchString, searchLocation) {
     let promise = search(searchString, searchLocation);
     promise.then(function(response) {
+      if (response.data.length == 0){
+        $('.showError').text('Im sorry there are no doctors available!');
+      }
       response.data.forEach(function(doctor) {
         $('.doctors').text(`There are ${response.meta.total} doctor(s) that match your request`);
-        console.log(response.meta.total);
-        if (response.meta.total == 0) {
-          $('.showError').text('Im sorry there are no doctors available!');
-        }
-         else {
           $('.showResults').append(`<div class="col-md-6 center"><h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3><h5>${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state}</h5><p>Phone: ${doctor.practices[0].phones[0].number}</p><p>${doctor.practices[0].website}</p><p>${doctor.profile.bio}</p><p>Accepts New Patients: ${doctor.practices[0].accepts_new_patients}</p><hr></div>`);
-           }
         });
     }, handleErrors);
   }
 
+
+
   function searchByName(name) {
     let promise = searchName(name);
     promise.then(function(response) {
-      console.log("this line reached");
+      if (response.data.length == 0){
+        $('.showError').text('Im sorry there are no doctors available!');
+      }
       response.data.forEach(function(doctor) {
-        console.log("second line reached");
-        if (response.data.length == 0) {
-          console.log(response.data.length);
-          $('.showError').text('Im sorry there are no doctors available!');
-          }
-          else {
-          $('.showResults').append(`<div class="col-md-6 center"><h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3><h5>${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state}</h5><p>Phone: ${doctor.practices[0].phones[0].number}</p><p>${doctor.practices[0].website}</p><p>${doctor.profile.bio}</p><p>Accepts New Patients: ${doctor.practices[0].accepts_new_patients}</p><hr></div>`);
-        }
-    }, handleErrors);
-  });
+        $('.showResults').append(`<div class="col-md-6 center"><h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3><h5>${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state}</h5><p>Phone: ${doctor.practices[0].phones[0].number}</p><p>${doctor.practices[0].website}</p><p>${doctor.profile.bio}</p><p>Accepts New Patients: ${doctor.practices[0].accepts_new_patients}</p><hr></div>`);
+      }, handleErrors);
+    });
   }
 
   function handleErrors(error) {
@@ -49,7 +43,10 @@ $(document).ready(function() {
     let searchLocation = $('#inputLocation').val();
     $('#inputSymptom').val("");
     $('#inputLocation').val("");
+    $('#searchName').val("");
     $(".showResults").empty();
+    $(".doctors").empty();
+    $(".showError").empty();
     searchDoctors(searchString, searchLocation);
   });
 
@@ -58,6 +55,8 @@ $(document).ready(function() {
     let searchName = $('#searchbar').val();
     $('#searchBar').val("");
     $(".showResults").empty();
+    $(".doctors").empty();
+    $(".showError").empty();
     searchByName(searchName);
   });
 });
